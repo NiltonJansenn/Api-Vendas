497d9e3f852c2af86d709545c4d66f3114499bf7
NiltonJansenn <nilton.jansenn@gmail.com>
2022-01-04 07:55:14 -0300

    Api-vendas-Configuracao

---
+++ src/shared/http/server.ts
@@ -0,0 +1,34 @@
+import express, { NextFunction, Request, response, Response } from 'express';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/erros/AppError';
+import { MessageChannel } from 'worker_threads';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, resṕonse: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        staus: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      mesage: 'erro interno do servidor',
+    });
+  },
+);
+
+const port = 3333;
+
+app.listen(port, () => {
+  console.log(`servidor funcionado na porta ${port}`);
+});
